169ab89362726eb10c206c6b4bbb90a1a48c8704
Ayush kushwaha <128110607+ayushkkushwaha@users.noreply.github.com>
2026-07-08 03:38:42 +0530

    Time: 4 ms (45.59%), Space: 23.6 MB (83.9%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> res;
-        unordered_map<int, int> mp;
-        int refCount = nums.size() / 3;
 
-        for (int i = 0; i < nums.size(); i++) {
+        int vote1 = 0, vote2 = 0, cand1 = INT_MIN, cand2 = INT_MIN;
+        int n = nums.size();
 
-            mp[nums[i]]++;
-
-            if (mp[nums[i]] == refCount + 1) {
-                res.push_back(nums[i]);
+        for (auto i : nums) {
+            if (vote1 == 0 && cand2 != i) {
+                vote1 = 1;
+                cand1 = i;
+            } else if (vote2 == 0 && cand1 != i) {
+                vote2 = 1;
+                cand2 = i;
+            } else if (cand1 == i)
+                vote1++;
+            else if (cand2 == i)
+                vote2++;
+            else {
+                vote1--, vote2--;
             }
-
         }
+
+        vector<int> res;
+        vote1 = 0, vote2 = 0;
+
+        for (auto i : nums) {
+            if (cand1 == i)
+                vote1++;
+            else if (cand2 == i)
+                vote2++;
+        }
+
+        if (vote1 > n / 3)
+            res.push_back(cand1);
+        if (vote2 > n / 3)
+            res.push_back(cand2);
 
         return res;
     }
