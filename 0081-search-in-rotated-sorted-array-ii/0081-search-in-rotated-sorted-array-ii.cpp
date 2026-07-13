d02645d74ecaf5a6fd0766f27b08b9bc9d89acf1
Ayush kushwaha <128110607+ayushkkushwaha@users.noreply.github.com>
2026-07-14 01:09:24 +0530

    Time: 0 ms (100%), Space: 18 MB (80.26%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -18,13 +18,13 @@ public:
             }
 
             else if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid])
+                if (nums[left] <= target && target <= nums[mid])
                     right = mid - 1;
                 else
                     left = mid + 1;
             } else {
                 if (nums[mid] <= nums[right])
-                    if (nums[mid] < target && target <= nums[right])
+                    if (nums[mid] <= target && target <= nums[right])
                         left = mid + 1;
                     else
                         right = mid - 1;
