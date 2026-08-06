4d3ac5db94c2fc5c4eae44436503cea82af8ea02
Ayush kushwaha <128110607+ayushkkushwaha@users.noreply.github.com>
2026-08-06 20:44:59 +0530

    Time: 0 ms (100%), Space: 22.9 MB (56.64%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -13,17 +13,24 @@
 class Solution {
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
+        // helper function
         return helper(nums, 0, nums.size() - 1);
     }
 
     TreeNode* helper(vector<int>& nums, int left, int right) {
+
+        // if left crossed right then return
         if (right < left)
             return NULL;
 
+        // find mid
         int mid = left + (right - left) / 2;
+        // create a node
         TreeNode* root = new TreeNode(nums[mid]);
 
+        // check in left half
         root->left = helper(nums, left, mid - 1);
+        // check in right half
         root->right = helper(nums, mid + 1, right);
 
         return root;
