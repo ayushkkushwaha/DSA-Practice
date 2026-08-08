69c47afcaa963a5916970127fbba7ae9cef873e7
Ayush kushwaha <128110607+ayushkkushwaha@users.noreply.github.com>
2026-08-08 13:21:41 +0530

    Time: 17 ms (24.47%), Space: 15.6 MB (70.2%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -21,15 +21,20 @@ public:
 class Solution {
 public:
     void helper(Node* root, vector<int>& res) {
+        // If the current node is NULL, stop the recursion
         if (!root)
             return;
 
+        // Add the current node's value to the result
         res.push_back(root->val);
 
+        // Traverse all children of the current node
         for (auto curr : root->children) {
+            // Recursively visit each child
             helper(curr, res);
         }
     }
+
     vector<int> preorder(Node* root) {
         vector<int> res;
         helper(root, res);
