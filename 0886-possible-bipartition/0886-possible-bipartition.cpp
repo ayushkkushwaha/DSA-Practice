27d3e2033a449db15779f910eaad8a31f245cade
Ayush kushwaha <128110607+ayushkkushwaha@users.noreply.github.com>
2026-08-26 21:14:12 +0530

    Time: 65 ms (17%), Space: 76.3 MB (27.65%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& graph, vector<int>& colors, int node, int clr,
+             bool& res) {
+
+        // Assign the current node a color.
+        // We use only two colors: 0 and 1.
+        colors[node] = clr;
+
+        for (auto curr : graph[node]) {
+
+            // If the neighbour already has the same color,
+            // then two adjacent nodes have the same color,
+            // so the graph is NOT bipartite.
+            if (colors[curr] != -1 && colors[curr] == clr)
+                res = false;
+
+            // If the neighbour has not been colored yet,
+            // assign it the opposite color and continue DFS.
+            if (colors[curr] == -1)
+                dfs(graph, colors, curr, !clr, res);
+        }
+    }
+
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+
+        vector<vector<int>> graph(n + 1);
+
+        for (auto i : dislikes) {
+            int u = i[0];
+            int v = i[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+
+        // -1 = not colored
+        //  0 = color 0
+        //  1 = color 1
+        vector<int> colors(graph.size(), -1);
+
+        bool res = true;
+
+        // Graph may contain multiple disconnected components,
+        // so start DFS from every uncolored node.
+        for (int i = 0; i < colors.size(); i++) {
+            if (colors[i] == -1)
+                dfs(graph, colors, i, 1, res);
+        }
+
+        return res;
+    }
+};
