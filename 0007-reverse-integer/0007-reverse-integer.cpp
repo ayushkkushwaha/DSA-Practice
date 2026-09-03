5971e1d3ecea0cc58f37368c39350632efe3737e
Ayush kushwaha <128110607+ayushkkushwaha@users.noreply.github.com>
2026-09-03 22:43:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int reverse(int x) {
-        int isNeg = 1;
-        long long temp = x;
-        
-        if (x < 0) {
-            isNeg = -1;
-            temp = x * -1LL;
+
+        long long n = 0;
+
+        while (x) {
+            n = n * 10 + x % 10;
+            x /= 10;
         }
 
-        string s = to_string(temp);
-
-        int l = 0, r = s.size() - 1;
-
-        while (l <= r)
-            swap(s[l++], s[r--]);
-        temp = stoll(s) * isNeg;
-
-        if (temp > INT_MAX || temp < INT_MIN)
+        if (n > INT_MAX || n < INT_MIN)
             return 0;
-        
-        return static_cast<int>(temp);
+
+        return n;
     }
 };
